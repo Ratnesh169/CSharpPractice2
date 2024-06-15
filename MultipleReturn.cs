21fd0cb8909844bfde1ae67d379c0572c10be8e3
Ratnesh Kumar Singh <ratnesh10061999@gmail.com>
2024-06-16 01:40:01 +0530

    Add project files.

---
+++ MultipleReturn.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject
+{
+    internal class MultipleReturn
+    {
+        // Using Tuple
+        public static (int, string, bool) GetMultipleValues()
+        {
+            int number = 42;
+            string text = "Hello, World!";
+            bool flag = true;
+            return (number, text, flag);
+        }
+        
+        // Using Class
+        public static MultipleValues GetMultipleValuesClass()
+        {
+            return new MultipleValues
+            {
+                Number = 42,
+                Text = "Hello, World!",
+                Flag = true
+            };
+        }
+        // Using Struct
+       
+
+        public static MultipleValuesStruct GetMultipleValuesStruct()
+        {
+            return new MultipleValuesStruct
+            {
+                Number = 42,
+                Text = "Hello, World!",
+                Flag = true
+            };
+        }
+
+        // Using Dynamic
+
+        public static dynamic GetMultipleValuesDynamic()
+        {
+            return new
+            {
+                Number = 42,
+                Text = "Hello, World!",
+                Flag = true
+            };
+        }
+
+
+    }
+    public class MultipleValues
+    {
+        public int Number { get; set; }
+        public string Text { get; set; }
+        public bool Flag { get; set; }
+    }
+
+    public struct MultipleValuesStruct
+    {
+        public int Number { get; set; }
+        public string Text { get; set; }
+        public bool Flag { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PracticeProject
+{
+    internal class Program : MultipleReturn
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using Tuple");
+
+            var result = GetMultipleValues();
+            Console.WriteLine($"Number: {result.Item1}, Text: {result.Item2}, Flag: {result.Item3}");
+
+            Console.WriteLine("Using Class");
+
+            var resultClass = GetMultipleValuesClass();
+            Console.WriteLine($"Number: {resultClass.Number}, Text: {resultClass.Text}, Flag: {resultClass.Flag}");
+
+            Console.WriteLine("Using Struct");
+
+            var resultStruct = GetMultipleValuesStruct();
+            Console.WriteLine($"Number: {resultStruct.Number}, Text: {resultStruct.Text}, Flag: {resultStruct.Flag}");
+
+            Console.WriteLine("Using Dynamic");
+
+            dynamic resultDynmic = GetMultipleValuesDynamic();
+            Console.WriteLine($"Number: {resultDynmic.Number}, Text: {resultDynmic.Text}, Flag: {resultDynmic.Flag}");
+
+        }
+    }
+}
